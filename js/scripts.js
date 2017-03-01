bd6c099577e41fa594bf755a333c533d02a630d4
Blake Womack and Christian Martinez <christian@>
2017-02-28 16:06:20 -0800

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,20 @@ $(document).ready(function() {
     var name = $("input#name").val();
     var deposit = $("input#initialDeposit").val();
     var newAccount = new accountSetup(name, deposit);
-    console.log(newAccount);
+    console.log(deposit);
+    $(".output").text(deposit);
+
+  $("#accountForm").click(function(){
+      var newDeposit = $("input#deposit").val();
+      var withdrawal = $("input#withdrawal").val();
+      var changeBalance = new balanceChange(newDeposit, withdrawal);
+      console.log(changeBalance);
+      newAccount.money.push(changeBalance);
+      console.log(newAccount);
+      var newTotal = newAccount.newTotal();
+      console.log(newTotal);
+      $(".output").text(newTotal);
+    });
   });
 });
 
@@ -13,4 +26,14 @@ $(document).ready(function() {
 function accountSetup(name, deposit) {
   this.name = name;
   this.deposit = deposit;
+  this.money = [];
+}
+
+function balanceChange(newDeposit, withdrawal) {
+  this.newDeposit = newDeposit;
+  this.withdrawal = withdrawal;
+}
+
+accountSetup.prototype.newTotal = function () {
+  return this.name + this.money
 }
